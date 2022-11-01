b37569d7785540351a54bdb5fdf5b0a7514faf57
rohraut <rohanraut372@gmail.com>
2022-11-01 13:16:57 +0530

    my first commit of 1 nov

---
+++ src/main/java/d1/HomePage.java
@@ -0,0 +1,14 @@
+package d1;
+
+public class HomePage {
+	public static void main(String[] args) {
+	String s = "Hello World";
+	String s1 ="";
+	for (int i = s.length()-1; i >=0; i--) {
+		s1 = s1+s.charAt(i);
+		
+	}
+	System.out.println(s1);
+	}
+
+}
